60c5f82e8cfba3b8efad2333ddce775509e90dc7
Bruce W. Herr II <bherr@indiana.edu>
2023-08-01 09:38:04 -0400

    Compile metadata for a release

--- rel-tools/mk-hra-metadata.js
+++ rel-tools/mk-hra-metadata.js
@@ -4,8 +4,9 @@ const sh = require('shelljs');
 const yaml = require('js-yaml');
 
 const hraVersion = process.argv.length > 2 ? process.argv[2] : 'v1.4';
-
 const hraRaw = `../scratch/digital-objects/collection/hra/${hraVersion}/raw`;
+const hraMeta = `../scratch/hra-${hraVersion}-metadata.json`;
+
 sh.mkdir('-p', hraRaw);
 const digitalObjects = sh
   .ls('../scratch/digital-objects/*/*/*/raw/metadata.yaml')
@@ -16,3 +17,15 @@ fs.writeFileSync(
   path.resolve(hraRaw, 'digital-objects.yaml'),
   yaml.dump({ ['digital-objects']: digitalObjects })
 );
+
+const results = [];
+for (const doPath of digitalObjects) {
+  const doMetaFile = `../scratch/digital-objects/${doPath}/raw/metadata.yaml`;
+  const metadata = yaml.load(fs.readFileSync(doMetaFile).toString());
+  const [ type, name, version ] = doPath.split('/');
+  results.push({
+    type, name, version, ...metadata
+  });
+}
+
+fs.writeFileSync(hraMeta, JSON.stringify(results, null, 2));
