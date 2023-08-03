a0f78007dee800480ff305f6d6065501582846e5
Bruce W. Herr II <bherr@indiana.edu>
2023-08-03 16:24:45 -0400

    Added code that generates jsonld for the ftu explorer (and proto-hra 2.0)

---
+++ rel-tools/mk-2d-ftu-objects.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const glob = require('glob');
+const Papa = require('papaparse');
+
+const output = '../scratch/2d-ftu-illustrations.jsonld';
+const baseIri = 'https://purl.humanatlas.io/2d-ftu/';
+const MAPPING = '../v1.4/2d-ftu/asct-b-2d-models-crosswalk.csv';
+
+const iriLookup = {};
+const refIllustrations = {};
+
+const MIMETYPES = {
+  svg: 'image/svg+xml',
+  ai: 'application/pdf',
+  png: 'image/png',
+};
+
+function getIllustrationFiles(iri, id) {
+  const files = glob.sync(`../v1.*/2d-ftu/2d-ftu-${id}.*`);
+  files.reverse();
+  return files.map((f) => {
+    const ext = f.split('.').slice(-1)[0];
+    const url = `https://hubmapconsortium.github.io/ccf-releases/${f.slice(3)}`;
+    return {
+      '@id': `${iri}#${ext}_file`,
+      '@type': 'FtuIllustrationFile',
+      file: url,
+      file_format: MIMETYPES[ext] || ext,
+    };
+  });
+}
+
+const lookupRows = Papa.parse(fs.readFileSync('2d-ftu-lookup.csv').toString(), {
+  header: true,
+  skipEmptyLines: true,
+}).data;
+for (const row of lookupRows) {
+  const iri = `${baseIri}${row.id}`;
+  iriLookup[row.old_id] = iri;
+  refIllustrations[iri] = {
+    '@id': iri,
+    '@type': 'FtuIllustration',
+    representation_of: row.representation_of,
+    label: row.label,
+    organ_id: '',
+    organ_label: '',
+    illustration_files: getIllustrationFiles(iri, row.id),
+  };
+}
+
+const mappingText = fs
+  .readFileSync(MAPPING)
+  .toString()
+  .split('\n')
+  .slice(10)
+  .join('\n');
+const mappingRows = Papa.parse(mappingText, { header: true }).data;
+for (const row of mappingRows) {
+  const iri = iriLookup[row['anatomical_structure_of']];
+  const illustration = (refIllustrations[iri] = refIllustrations[iri] || {});
+
+  illustration.organ_id = row.organ_id;
+  illustration.organ_label = row.organ_label;
+
+  const mapping = (illustration.mapping = illustration.mapping || []);
+  mapping.push({
+    '@id': `${iri}#${row.node_name}`,
+    '@type': 'FtuIllustrationNode',
+    svgId: row.node_name,
+    label: row.label,
+    representation_of: row.representation_of,
+  });
+}
+
+const results = Object.values(refIllustrations);
+const jsonld = {
+  '@context':
+    'https://hubmapconsortium.github.io/ccf-ontology/ccf-context.jsonld',
+  '@graph': results,
+};
+
+fs.writeFileSync(output, JSON.stringify(jsonld, null, 2));
